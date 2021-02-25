daacc589ac9ca1ec3de72f5b266d7693b50c7c5d
Daniele Albanese <daniele.albanese98@gmail.com>
2021-02-25 23:59:37 +0100

    user_model.py -> Fixed errors + Added function exist

--- src/models/user_model.py
+++ src/models/user_model.py
@@ -1,15 +1,15 @@
 import sqlite3
+
+from . import constant
 from src.services.logging_service import LoggingService
 
 logging_service = LoggingService(name=__name__, formatter=None, datefmt=None, file_handler=None)
 
 logger = logging_service.get_logger()
 
-DB_NAME = "bot.db"
-
 
 class Schema:
-    def __init__(self, db_name=DB_NAME):
+    def __init__(self, db_name=constant.DB_NAME):
         self.conn = sqlite3.connect(db_name)
         self.curs = self.conn.cursor()
         self.create_user_table()
@@ -32,16 +32,14 @@ class Schema:
 
 
 class UserModel:
-    USER_TABLE_NAME = "user"
-
-    def __init__(self, db_url=DB_NAME):
+    def __init__(self, db_url=constant.DB_NAME):
         self.conn = sqlite3.connect(db_url)
         self.conn.row_factory = sqlite3.Row
         self.curs = self.conn.cursor()
 
     def create(self, id, first_name, last_name, username):
         query = """
-                INSERT INTO user(id, fist_name, last_name, username)
+                INSERT INTO user(id, first_name, last_name, username)
                 VALUES(?, ?, ?, ?)
                 """
 
@@ -86,6 +84,45 @@ class UserModel:
             self.conn.commit()
 
             return True
+        except sqlite3.Warning as e:
+            logger.exception("UserModel -> delete -> Warning -> " + str(e))
+
+            return False
+        except sqlite3.Error as e:
+            logger.exception("UserModel -> delete -> Error -> " + str(e))
+
+            return False
+        except sqlite3.IntegrityError as e:
+            logger.exception("UserModel -> delete -> IntegrityError -> " + str(e))
+
+            return False
+        except sqlite3.ProgrammingError as e:
+            logger.exception("UserModel -> delete -> ProgrammingError -> " + str(e))
+
+            return False
+        except sqlite3.OperationalError as e:
+            logger.exception("UserModel -> delete -> OperationalError -> " + str(e))
+
+            return False
+        except sqlite3.NotSupportedError as e:
+            logger.exception("UserModel -> delete -> NotSupportedError -> " + str(e))
+
+            return False
+
+    def exist(self, id):
+        query = """
+                SELECT EXISTS (
+                    SELECT username
+                    FROM user 
+                    WHERE id=?
+                )
+                """
+
+        try:
+            res = self.curs.execute(query, (id,)).fetchone()
+            self.conn.commit()
+
+            return bool(res[0])
         except sqlite3.Warning as e:
             logger.exception("UserModel -> delete -> Warning -> " + str(e))
 
